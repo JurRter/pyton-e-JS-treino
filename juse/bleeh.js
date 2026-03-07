f37c57108a9e840aaf0abe84e3fe814424360c6a
JurRter <brendo.henrique@mail.uft.edu.br>
2026-03-07 01:02:46 -0300

    primeira commit

---
+++ juse/bleeh.js
@@ -0,0 +1,8 @@
+console.log('hellooo');
+function soma(a, b) {
+    
+    
+    
+    
+    return a + b;
+}
---
+++ piton/hello.py
@@ -0,0 +1,19 @@
+def soma(a, b):
+    return a + b
+
+def data():
+    dia = input("Que dia você nasceu")
+    mes = input("Qual mes você nasceu")
+    ano = input("Qual ano você")
+    print(f"{dia}/{mes}/{ano}")
+    
+def tchurusbangos():
+    nome = input("Qual teu nome ze ")
+    idade = input("Qual tua idade ")
+    peso = input("Qual teu peso ")
+
+    print(f"seu nome é {nome}\nsua idade: {idade}\nseu peso: {peso}")
+
+if __name__ == "__main__":
+    data()
+
